1d2602e8fc217ac12390248cffe8ceeebef16d3f
lydiaanzures <lydiaanzures@gmail.com>
2022-01-27 20:52:53 -0500

    Initial Commit

---
+++ Shared/swift_fruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_fruitsApp.swift
+//  Shared
+//
+//  Created by Lydia Anzures on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_fruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
